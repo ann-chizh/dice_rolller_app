416186e218b20fd204c2d704c6e49fb72cfe9c27
Ann <chizhanya2003@gmail.com>
2023-12-10 23:49:05 +0300

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_course/gradient_container.dart';
+
+void main() {
+  runApp(
+      MaterialApp(
+      home: Scaffold(
+        body:  GradientContainer.purple(),
+      ),
+    )
+  );
+}
+
+
